0ea488c20833c94541d3bc5842357e7f1b8d608a
alsongard <alsongard@gmail.com>
2024-02-12 16:17:24 -0500

    functions

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 // ReactDOM.render(<h1>Hello Everyone!</h1>, document.getElementById("root"))
-ReactDOM.render(
-    <ul><li>Gard Alson</li><li>Age is 23</li></ul>,
-    document.getElementById("root"))//was working place comma @right placef
+// ReactDOM.render(
+//     <ul><li>Gard Alson</li><li>Age is 23</li></ul>,
+//     document.getElementById("root"));
+    //was working place comma @right placef
 // listOfElements.foreach(el => ReactDOM.render(<ul><li>Gard</li><li>Alson</li></ul>, document.getElementById("root"))) not work
 let people = [
     "Gard Alson Safari",
@@ -11,3 +12,29 @@ let people = [
 // //used the map() attribure for the array people which seems to iterate throught the list
 // const listItems = people.map(person => <li>{person}</li>);
 // ReactDOM.render(listItems, document.getElementById("root")); 
+
+function Profile(){
+    return (
+        <ul>
+            <li>Partin Ways</li>
+            <li>Distraction</li>
+        </ul>
+    );
+};
+function MainContent(){
+    return (
+        <div>
+            <h1>You there let's see</h1>
+            <p>Hi there let's see again</p>
+
+        </div>
+    );
+};
+//these can be written in one line function MainComponent(){ return <h1>You there</h1>}
+ReactDOM.render(
+    <div>
+        <Profile/>
+        <MainContent/>
+    </div>,
+    document.getElementById("root")
+);
