30e9bec4d977077750cdcb120cccee0604210e7d
alsongard <alsongard@gmail.com>
2024-04-18 17:31:00 -0400

    working with npx adding icons next

---
+++ all_projects/src/profile.js
@@ -0,0 +1,45 @@
+import userImage from "./swordGun.jpg"
+import "./profile.css"
+function Home(){
+    return(
+        <section className="home">
+            <div className="container">
+                <img className="user-img" src={userImage} alt="user-image"/>
+                <h1>Gard Alson</h1>
+                <p>Full Stack Developer</p>
+                <div className="socialIcons">
+                    <a href="#">Twitter</a>
+                    <a href="#">Github</a>
+                </div>
+            </div>
+        </section>
+    )
+}
+function About(){
+    return(
+        <section className="about">
+            <h2>About</h2>
+            <p>Lorem Ispum Lorem Ispum Lorem Ispum Lorem</p>
+        </section>
+    )
+}
+function Interest(){
+    return(
+        <section className="interest">
+            <h2>Interests</h2>
+            <p>I am interested in Software Development, Web Development both back-end and front-end, Cyber Security, Data Science , Android Development and Networking.</p>
+        </section>
+    )
+}
+
+function Page(){
+    return(
+        <div>
+            <Home/>
+            <About/>
+            <Interest/>
+        </div>
+
+    )
+}
+export default Page;
