8b64a95b91dab519d2ace4edffff80178e7e26df
alsongard <alsongard@gmail.com>
2024-04-29 08:28:56 -0400

    hero section finished and images

--- all_projects/src/Components/AirBnB.js
+++ all_projects/src/Components/AirBnB.js
@@ -1,4 +1,6 @@
 import logo from "../images/AirbnbLogo.jpg"
+import bnbImages from "../images/AirBnB.png"
+import "../CSS/airbnb.css"
 function NavBar(){
     return (
         <header>
@@ -6,10 +8,24 @@ function NavBar(){
         </header>
     )
 }
+function Hero(){
+    return(
+        <section className="hero">
+            <div className="hero-content">
+                <img className="bnbStack" src={bnbImages} alt="airbnb images"/>
+                <div className="hero-text">
+                    <h1>Online Experiences</h1>
+                    <p>Join unique interactive activities lead by one-of-a-kind hosts-all without leaving home.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
 function Page(){
     return(
         <div>
             <NavBar/>
+            <Hero/>
         </div>
     )
 }
