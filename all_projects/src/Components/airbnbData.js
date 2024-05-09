d965936d719a6865bda4951cdc2f64f2952a4c7a
alsongard <alsongard@gmail.com>
2024-05-08 20:11:21 -0400

    airbnb data

--- all_projects/src/Components/airbnbData.js
+++ all_projects/src/Components/airbnbData.js
@@ -1,8 +1,54 @@
-export default [
+export default varita[
     {
-
+        id: 1,
+        title: "Learn Web Development",
+        description: "Interested in becoming a Web developer? For begineer and experienced develers alike, join us.",
+        price : 100,
+        coverImage: "provide_file_path_to_image",
+        stats: {
+            rating: 4.6, 
+            reviewCount: 20            
+        },
+        location: "Physical",
+        openSpots: 17        
     },
     {
-        
+        id: 2,
+        title: "Learn Software Development",
+        description: "Interested in becoming a Software developer? For begineer and experienced develers alike, join us.",
+        price : 150,
+        coverImage: "provide_file_path_to_image",
+        stats: {
+            rating: 5.6, 
+            reviewCount: 30            
+        },
+        location: "Online",
+        openSpots: 27
+    },
+    {
+        id: 3,
+        title: "Learn Data Science",
+        description: "Interested in becoming a Data Scientist? For begineer and experienced develers alike, join us.",,
+        price : 145,
+        coverImage: "provide_file_path_to_image",
+        stats: {
+            rating: 8.6, 
+            reviewCount: 25            
+        },
+        location: "Online",
+        openSpots: 27
+    },
+    {
+        id: 4,
+        title: "Learn Machine Learning",
+        description: "Interested in becoming a Machine Learning Engineer? For begineer and experienced develers alike, join us.",
+        price : 200,
+        coverImage: "provide_file_path_to_image",
+        stats: {
+            rating: 9.6, 
+            reviewCount: 35            
+        },
+        location: "Physical",
+        openSpots: 17
     }
 ]
