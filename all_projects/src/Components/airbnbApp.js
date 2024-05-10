2ac89fe4fd12a54e15237bd810da3ad41905da81
alsongard <alsongard@gmail.com>
2024-05-09 20:21:29 -0400

    use of objects

--- all_projects/src/Components/airbnbApp.js
+++ all_projects/src/Components/airbnbApp.js
@@ -2,9 +2,26 @@ import airbnbData  from "./airbnbData";
 import NewCard from "./new_card_component";
 
 function AirbnbApp(){
+        const cardElements = airbnbData.map(function(cardItem){
+            return  <NewCard key={cardItem.id} item={cardItem}/>
+        })
+        return cardElements
+    } 
+/*function AirbnbApp(){
     const cardElements = airbnbData.map(function(cardItem){
-        return  <NewCard key={cardItem.id} slots={cardItem.openSpots} mainimage={cardItem.coverImage} rating={cardItem.stats.rating} country={cardItem.location} message={cardItem.description} cost={cardItem.price} spots={cardItem.openSpots} review={cardItem.stats.reviewCount}/>
+        return  <NewCard
+        key={cardItem.id} 
+        slots={cardItem.openSpots} 
+        mainimage={cardItem.coverImage} 
+        rating={cardItem.stats.rating} 
+        country={cardItem.location} 
+        message={cardItem.description} 
+        cost={cardItem.price} 
+        spots={cardItem.openSpots} 
+        review={cardItem.stats.reviewCount}
+        />
     })
     return cardElements
-}
+} 
+*/
 export default AirbnbApp
--- all_projects/src/Components/new_card_component.js
+++ all_projects/src/Components/new_card_component.js
@@ -2,32 +2,33 @@ import { FaStar } from "react-icons/fa6";
 import "../CSS/airbnb.css";
 
 function NewCard(prop){
+    // console.log(prop.item)
     let badgeCard ;
-    if (prop.slots === 0){
+    if (prop.item.openSpots === 0){
         badgeCard = "SOLD OUT";
     }
-    else if (prop.country === "Online"){
+    else if (prop.item.location === "Online"){
         badgeCard = "ONLINE";
     }
     return (
         <div className="card-sub">
             {badgeCard &&  <div className="slots"> <p>{badgeCard}</p></div>}
-            <img src={prop.mainimage} alt="trainer"/>
+            <img src={prop.item.coverImage} alt="trainer"/>
             <div className="info-group">
-                <a href="https://"><FaStar/> {prop.rating} ({prop.review})</a>
-                <p>{prop.country}</p>
+                <a href="https://"><FaStar/> {prop.item.stats.rating} ({prop.item.stats.reviewCount})</a>
+                <p>{prop.item.location}</p>
             </div>  
             <div className="info-group">
-                <p>{prop.message}</p>
+                <p>{prop.item.description}</p>
             </div>
             <div className="info-group">
                 <div className="holder">
                     <p>Available spots</p>
-                    <p> {prop.spots}</p>
+                    <p> {prop.item.openSpots}</p>
                 </div>
                 <div className="holder">
                     <p>Price</p>
-                    <p>{prop.cost} $</p>
+                    <p>{prop.item.price} $</p>
                 </div>
                 
             </div>
