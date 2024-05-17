e2aebdcf2d4413ac84699f6736e1943f6ccef246
alsongard <alsongard@gmail.com>
2024-05-17 12:51:51 -0400

    tourist data collected

---
+++ all_projects/src/Components/tourist-data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        title: "Mountain Fuji",
+        location: "Japan",
+        googeMapsUrl: "https...",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description:"Mount Fuji is the single most popular tourist site in Japan, for both Japanase and foreign tourists.",
+        imageUrl: "htpps://....."
+    },
+    {
+        title: "Dubai",
+        location: "UAE",
+        googeMapsUrl: "https...",
+        startDate: "13 Dec, 2022",
+        endDate: "29 Dec, 2022",
+        description:"Dubai is a city and emirate in the United Arab Emirates known for luxury shopping, ultramodern architecture and a lively nightlife scene.",
+        imageUrl: "htpps://....."
+    },
+    {
+        title:"Ifel Tower",
+        location:"Paris",
+        googeMapsUrl: "https...",
+        startDate: "16 Aug, 2023",
+        endDate: "27 Sep, 2023",
+        description:"The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France.",
+        imageUrl:"htpps://....."
+    },
+    {
+        title:"Statue of Liberty",
+        location:"New York",
+        googeMapsUrl: "https...",
+        startDate: "1 Jan, 2024",
+        endDate: "14 Jan, 2024",
+        description:"The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island in New York Harbor in New York City.",
+        imageUrl:"htpps://....."
+    }
+]
