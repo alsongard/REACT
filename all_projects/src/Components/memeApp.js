012ed5e06e233076de5fad0109dd891ab1cd1ae7
alsongard <alsongard@gmail.com>
2024-06-11 15:09:44 -0400

    learning react use state

--- all_projects/src/Components/memeApp.js
+++ all_projects/src/Components/memeApp.js
@@ -1,17 +1,37 @@
 import React from "react";
 import MemesData from "./memesData.js";
+import memesData from "./memesData.js";
 
 function Meme(){
     // function handler(){
     //     console.log("Welcome to Project Meme!")
     // }
-    const[memeImage, setMemeImage] = React.useState("");
-    function getMemeImage(){
+    /*function getMemeImage(){
         const memeArray = MemesData.data.memes;
-        // console.log(memeArray);
+        console.log(memeArray);
         const randomIndex = Math.floor(Math.random() * memeArray.length);
-        // console.log(randomIndex);
+        console.log(randomIndex);
         setMemeImage(memeArray[randomIndex].url);
+    }*/
+    const [meme, setMeme] = React.useState({
+        topText : "",
+        bottomText: "",
+        randomImage : "https://i.imgflip.com/30b1gx.jpg"
+    });
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+    const[memeImage, setMemeImage] = React.useState("https://i.imgflip.com/30b1gx.jpg");
+    function getMemeImage(){
+        const memesArray = allMemeImages.data.memes;
+        const randomIndexNumber = Math.floor(Math.random() * memesArray.length);
+        // console.log(randomIndexNumber);
+        const url = memesArray[randomIndexNumber].url;
+        // console.log(url);
+        setMeme((prevMeme)=>{
+            return {
+                ...prevMeme, 
+                randomImage : url
+            }
+        } );
     }
     return(
         <section className="home">
@@ -23,7 +43,7 @@ function Meme(){
             </form>
             <button onClick={getMemeImage}>Get new meme image</button>
             <div className="imageHolder">
-                <img src={memeImage} className="memeImg" alt="meme"/>
+                <img src={meme.randomImage} className="memeImg" alt="meme"/>
             </div>
         </section>
     )
--- all_projects/src/index.js
+++ all_projects/src/index.js
@@ -8,24 +8,25 @@ import ReactDOM from 'react-dom/client';
 // import JokeApp  from './Components/jokeApp.js';
 // import App from './App';
 // import MemePage from './Components/memePage.js';
-// import TouristPage from './Components/touristPage.js';
+import TouristPage from './Components/touristPage.js';
 // import  ProfilePage from "./profile";
-import Challenges  from './Components/challenges.js';
+// import ProfileUser from './Components/reactObjects.js';
+// import Challenges  from './Components/challenges.js';
 
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Challenges/>
+    <TouristPage/>
   </React.StrictMode>
 );
+reportWebVitals();
 //<MemePage/>
  
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 /*
 <ContactUnit
       img="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"
