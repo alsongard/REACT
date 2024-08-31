12bd34d4e8b03249d8cf7ece1b058d826f3550f9
alsongard <alsongard@gmail.com>
2024-08-30 20:14:20 -0400

    derived state which is not preffered

--- all_projects/src/Components/boxComponent.js
+++ all_projects/src/Components/boxComponent.js
@@ -1,11 +1,27 @@
+// passed props.on value to React.useState() in order to chang it's value
+import React from "react";
 function BoxComponent(props)
 {
+    const [onValue, setOnValue] = React.useState(props.on); //derived state
+    function changeValue()
+    {
+        setOnValue((onValue)=>{
+            if (onValue === false)
+            {
+                return true;
+            }
+            else if(onValue === true)
+            {
+                return false;
+            }
+        })
+    }
     const style = 
     {
-        backgroundColor: props.on ? "#222222" : "none"
+        backgroundColor: onValue ? "#222222" : "transparent"
     };
     return (
-        <div style={style}  className="box">
+        <div style={style}  className="box" onClick={changeValue}>
         </div>
     )
 }
