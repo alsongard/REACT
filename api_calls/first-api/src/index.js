58229d786e1b9c4d9f6f7a2d4381c9585079a772
alsongard <alsongard@gmail.com>
2024-09-07 09:09:42 -0400

    addition  of observingDependancy and MemePage

--- api_calls/first-api/src/index.js
+++ api_calls/first-api/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
-import StarWarsApi from './components/starwars_api';
+// import StarWarsApi from './components/starwars_api';
 import MemePage from './components/memePage';
-import ObserveDependency from './components/dependency_array';
+// import ObserveDependency from './components/dependency_array';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
