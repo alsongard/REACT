ee38a57e3e5ed7a50bf61e1ec2fe0209a35290e9
alsongard <alsongard@gmail.com>
2024-11-02 08:28:05 -0400

    useState()

--- state_management/src/components/1-state-intro.js
+++ state_management/src/components/1-state-intro.js
@@ -3,17 +3,37 @@ import React from "react";
 
 function ListNames(){
     const [listNames , setListNames] = React.useState(["Jack", "Chucky", "Liena"]);
+    const [names, setNames] = React.useState("");
+
+    
+    console.log(`The value of names is ${names}`)
+    
+    function addName(){
+        setListNames((prevList)=>{
+            return [...prevList, names];
+        });
+    }
+    
+    function handleChange(event){
+        console.log(event.target.value);
+        setNames(event.target.value);
+    };
+
     const nameElements = listNames.map((dataItem)=>{
         return (
-            <ul>
+            <ul className="list-disc ml-[20px]">
                 <li>{dataItem}</li>
             </ul>
         )
     });
 
+
     return (
         <div>
-            <p>Hello World</p>
+            <div className="my-[20px]">
+                <input type="text" onChange={handleChange}  value={names}/>
+                <button className="ml-[15px] bg-sky-300"  onClick={addName}>Add Name</button>
+            </div>
             {nameElements}
         </div>
     )
@@ -21,20 +41,15 @@ function ListNames(){
 
 function Introduction2State(){
     const [count, setCount] = React.useState(10);
-    const [names, setNames] = React.useState("");
     
     // render the list Names
     function addNumber(){
         setCount(count + 1);
     };
-    function handleChange(event){
-        console.log(event.target.value);
-    };
-
+   
     return (
-        <div className="bg-slate-400">
+        <div>
             <p  onClick={addNumber}  >Count = {count} </p>
-            <input type="text" onChange={handleChange} name={names} value={names}/>
         </div>
     )
 }
@@ -44,7 +59,9 @@ function AllApps()
     return (
         <>
             <Introduction2State/>
-            <ListNames/>
+            <div className="m-[50px]">
+                <ListNames/>
+            </div>
         </>
     )
 }
