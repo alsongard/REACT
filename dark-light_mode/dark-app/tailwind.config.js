49d2c6e08cd583702da746da0ce4e96ecd9027eb
alsongard <alsongard@gmail.com>
2024-11-22 00:37:02 +0300

    tailwind setup and styles

--- dark-light_mode/dark-app/tailwind.config.js
+++ dark-light_mode/dark-app/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/*/*.{js, tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: 'class'
 }
 
