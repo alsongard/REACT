aea7cfa255be549a5157bf9fbeca5826c9db728c
alsongard <alsongard@gmail.com>
2025-02-12 00:22:36 +0200

    Laptop Selling Website Started Working on it

---
+++ the_preloader/src/components/page.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Preloader from './preloader';
+import Header from './header';
+
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+
+export default function Page() {
+    const [isLoading, setIsLoading] = React.useState(true);
+    React.useEffect(()=>{
+        const timer = setTimeout(()=>{
+            setTimeout(()=>{
+                setIsLoading(false)
+            }, 5000)
+        })
+        return  ()=>{clearTimeout(timer)};
+    },[])
+
+    return (
+        <div>
+            {isLoading ? (<Preloader/> ): (
+                <main className="page ">
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<div><Header/> <Outlet/> </div>}>
+
+                            </Route>
+                        </Routes>
+
+                    
+                    </BrowserRouter>
+                </main>
+            )}
+        </div>
+    )
+}
+
+
+/**
+ *                         <Header/>
+                        <section>
+                            <h1>The Preloader</h1>
+                            <p>Lorem ispum</p>
+                        </section>
+ */
