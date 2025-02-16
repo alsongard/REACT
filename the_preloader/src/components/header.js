99e74aba66a496e711d86ea7c8635a65742fc23a
alsongard <alsongard@gmail.com>
2025-02-16 22:07:00 +0200

    header NavLinks update and active className prop

--- the_preloader/src/components/header.js
+++ the_preloader/src/components/header.js
@@ -1,31 +1,58 @@
 import { NavLink } from "react-router-dom"
 export default function Header() {
   return (
-    <header>
-        <h1>The Logo</h1>
-
-
-        <ul>
+    <header className="flex flex-row absolute  h-[70px] w-[100vw] justify-between pl-[20px] pr-[30px] items-center">
+        <h1 className="text-white text-[30px]">The Logo</h1>
+        <ul className="flex flex-row w-[600px] px-[20px] bg-gradient-to-r from-sky-500 to-indigo-500 rounded-md py-[10px] justify-between">
           <li>
-            <NavLink to="#">
+            <NavLink to="/" 
+              className={({isActive})=>
+                isActive ? "text-black" : "text-white"}
+            >
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="#">
+            <NavLink to="/about"
+              className={({isActive})=>
+                isActive ? "text-black" : "text-white"}
+            >
               About
             </NavLink>
           </li>
           <li>
-            <NavLink to="#">
+            <NavLink to="/services"
+              className={({isActive})=>
+                isActive ? "text-black" : "text-white"}
+            >
               Services
             </NavLink>
           </li>
           <li>
-            <NavLink to="#">
+            <NavLink to="/contact"
+              className={({isActive})=>
+                isActive ? "text-black" : "text-white"}
+            >
               Contact
             </NavLink>
           </li>
+          <li>
+            <NavLink to="/contact"
+              className={({isActive})=>
+                isActive ? "text-black" : "text-white"}
+            >
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact"
+              className={({isActive})=>
+                isActive ? "text-black" : "text-white"}
+            >
+              Sign in
+            </NavLink>
+          </li>
+          
         </ul>
     </header>
   )
